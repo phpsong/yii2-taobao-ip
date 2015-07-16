4ce5c767869a0618e245e09a85ccf052767d864b
古俊杰 <dggugjj@163.com>
2015-07-16 17:17:01 +0800

    初始化项目

---
+++ IpComponent.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dggug
+ * Date: 2015/7/16
+ * Time: 17:16
+ */
+
+namespace api\taobao;
+
+use yii\base\Component;
+
+class IpComponent extends Component
+{
+
+}
