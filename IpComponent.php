53eb1b19bbe31d51175c22958a7944cbed1e7803
古俊杰 <dggugjj@163.com>
2015-07-17 10:48:04 +0800

    更新测试版

--- IpComponent.php
+++ IpComponent.php
@@ -8,9 +8,63 @@
 
 namespace api\taobao;
 
+use Exception;
+use Yii;
 use yii\base\Component;
+use yii\base\InvalidConfigException;
+use yii\caching\Cache;
 
 class IpComponent extends Component
 {
+    public $enableCache = true;
+    public $cache;
+    public $cacheKey = 'taobao_ip_api';
+    public $cacheDuration = 86400;
+    /**
+     * @var \yii\caching\Cache
+     */
+    private $_cache;
+    private $_url = 'http://ip.taobao.com/service/getIpInfo.php?ip=';
 
+    public function init()
+    {
+        parent::init();
+        if ($this->cache === null) {
+            $this->_cache = Yii::$app->cache;
+        } else {
+            $this->_cache = Yii::$app->get($this->cache);
+        }
+        if (!$this->_cache instanceof Cache) {
+            throw new InvalidConfigException("cache must be extends \\yii\\caching\\Cache");
+        }
+    }
+
+    /**
+     * 通过API查询IP信息
+     * @param null $ip
+     * @return IpData|mixed
+     */
+
+    public function get($ip = null)
+    {
+        $ip = $ip === null ? Yii::$app->request->userIP : $ip;
+        $cacheKey = $this->cacheKey . $ip;
+        $ipData = new IpData();
+        if ($this->enableCache && $json = $this->_cache->get($cacheKey)) {
+            $ipData->setAttributes(json_decode($json, true), false);
+            return $ipData;
+        }
+        $context = stream_context_create(['http' => ['timeout' => 1]]);
+        $url = $this->_url . $ip;
+        try {
+            $result = json_decode(file_get_contents($url, 0, $context), true);
+        } catch (Exception $e) {
+            $result = null;
+        }
+        if ($result && $result['code'] == 0) {
+            $ipData->setAttributes($result['data'], false);
+            $this->enableCache && $this->_cache->set($cacheKey, json_encode($ipData), $this->cacheDuration);
+        }
+        return $ipData;
+    }
 }
---
+++ IpData.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dggug
+ * Date: 2015/7/17
+ * Time: 10:13
+ */
+
+namespace api\taobao;
+
+
+use yii\base\Model;
+
+class IpData extends Model
+{
+    public $ip;
+    public $country;
+    public $area;
+    public $region;
+    public $city;
+    public $county;
+    public $isp;
+    public $area_id;
+    public $isp_id;
+    public $county_id;
+    public $city_id;
+    public $region_id;
+    public $country_id;
+}
